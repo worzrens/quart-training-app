cc94af605de0c63c21c4c030c8e5ebc08643a175
Edgar Shport <edgar.shport@eleks.com>
2022-02-15 10:26:45 +0200

    Populate script, get+list routes for model

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import imp
 import quart.flask_patch
-from quart import Quart, jsonify
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import enum
+from quart import Quart
+from marshmallow_sqlalchemy import SQLAlchemySchema
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_marshmallow import Marshmallow
+from marshmallow_enum import EnumField
 
 user='psql_user'
 password='1'
@@ -12,30 +15,87 @@ port='5432'
 database='quart-app'
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
 
-
 app = Quart(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
 engine = create_engine(DATABASE_CONNECTION_URI)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+Session = sessionmaker(engine)
 
-class Switch(Base):
-    __tablename__ = 'switches'
-    id = Column(Integer, primary_key=True)
-    color = Column(String(80), unique=True, nullable=False)
-    type = Column(String(120), unique=True, nullable=False)
+class SwitchType(enum.Enum):
+    linear = 'Linear'
+    tactile = 'Tactile'
+    clicky = 'Clicky'
+
+
+class Switch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color = db.Column(db.String(80), unique=True, nullable=False)
+    type = db.Column(db.Enum(SwitchType), nullable=False)
+    company = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return '<Switch %r>' % self.color
+        return f'<{self.company}> {self.color}'
 
 
-@app.route('/')
-async def hello():
-    switches = db_session.query(Switch).all()
-    print(switches)
-    return '1'
+class SwitchSchema(SQLAlchemySchema):
+    class Meta:
+        model = Switch
+
+    id = ma.auto_field()
+    color = ma.auto_field()
+    type = EnumField(SwitchType)
+    company = ma.auto_field()
+
+
+switch_schema = SwitchSchema()
+switches_schema = SwitchSchema(many=True)
+
+def fill_db():
+    db.drop_all()
+    db.create_all()
+
+    with Session() as session:
+        mock_switches = [
+            Switch(color='Red', type=SwitchType.linear, company='MX Cherry'),
+            Switch(color='Silver', type=SwitchType.linear, company='MX Cherry'),
+            Switch(color='Black', type=SwitchType.linear, company='MX Cherry'),
+
+            Switch(color='Brown', type=SwitchType.tactile, company='MX Cherry'),
+            Switch(color='Clear', type=SwitchType.tactile, company='MX Cherry'),
+            Switch(color='Gray', type=SwitchType.tactile, company='MX Cherry'),
+
+            Switch(color='Blue', type=SwitchType.clicky, company='MX Cherry'),
+            Switch(color='Green', type=SwitchType.clicky, company='MX Cherry'),
+            Switch(color='White', type=SwitchType.clicky, company='MX Cherry'),
+        ]
+        session.bulk_save_objects(mock_switches)
+        session.commit()
+        return True
+
+
+@app.route('/switches/<int:id>')
+async def switches_get(id):
+    switch = Switch.query.get(id)
+    return switch_schema.dump(switch)
+
+@app.route('/switches')
+async def switches_list():
+    switches = Switch.query.all()
+    return {
+        "switches": switches_schema.dump(switches), 
+    }
+
+
+@app.route('/populate', methods=["POST"])
+async def populate():
+    print('Started population')
+    is_done = fill_db()
+    print(f'Finished population {is_done}')
+    print(Switch.query.all())
+    return 'Completed', 201
+
 
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    app.run()
+    db.create_all()
+    app.run(debug=True)
